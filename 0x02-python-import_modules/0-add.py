b67b5a376eab74e876f83eca8cb7b6c32a627a2b
amanita4t <amanueltekalign4@gmail.com>
2023-03-12 01:13:43 -0800

    my first module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
-def add(a, b)
+#import and perform addition
+from add_0 import add
 a = 1
 b = 2
-print('{} + {} = {}'.format(a, b, add)
+print('{} + {} = {}'.format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# Addition module
+def add(a, b):
+    return a + b
