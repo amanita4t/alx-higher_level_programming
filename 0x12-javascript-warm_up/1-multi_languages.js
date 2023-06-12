dceb2f921cf0eac48fc533d41c43e872eb0af1d0
amanita4t <amanueltekalign4@gmail.com>
2023-06-12 14:40:11 -0700

    minor fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const c = "C is fun"
-const python = "Python is cool"
-const java = "JavaScript is amazing"
-console.log(c)
-console.log(python)
-console.log(java)
+let cpj = 'C is fun\n' +
+'Python is cool\n' +
+'JavaScript is amazing\n'
+console.log(cpj)
